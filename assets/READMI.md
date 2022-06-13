d23765890107f2b71dcb8a5670c70f3c2d8e07d1
Thiago Lopes <thiagolnunes.2018@outlook.com>
2022-06-12 21:30:04 -0300

    iniciando sintaxe e operadores

---
+++ assets/READMI.md
@@ -0,0 +1,10 @@
+# Atividade proprosta no curso de JavaScript.
+
+Aprendendo sintaxe-operadores.
+
+## Atividade
+
+. crie uma função que receba dois números como parâmetros.
+. confira se os números são iguais.
+. confira se a soma dos númeors é maior que 10 ou menor que 20.
+retorne uma string dizendo "Os números num1 e num2 não/são iguais. Sua soma é soma, que é maior/menor que 10 e maior/menor que 20".
---
+++ assets/sintaxe-operadores.js
@@ -0,0 +1,34 @@
+function comparaNumeros (num1, num2) {
+    const primeiraFrase = criaPrimeiraFrase(num1, num2);
+    const segundaFrase = criaSegundaFrase(num1, num2);
+}
+
+function criaPrimeiraFrase(num1, num2) {
+    let saoIguais = '';
+
+    if(num1 !== num2) {
+        saoIguais = 'não';
+    }
+
+    return `Os números ${num1} e ${num2} ${saoiguais} são iguais. `
+}
+
+function criaSegundaFrase(num1, num2){
+    const soma = num1 + num2;
+
+    let reusltado10 = 'menor';
+    let reusltado20 = 'menor';
+    const compara10 = soma > 10;
+    const compara20 = soma > 20;
+
+    if(compara10) {
+        reusltado10 = 'maior';
+    }
+
+    if(compara20) {
+        reusltado20 = 'maior';
+    }
+
+    return 'Sua soma é ${soma}, que é ${resultado10} que 10 e ${resultado20} que 20.';
+}
+console.log(comparaNumeros(1, 2));
